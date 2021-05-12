fb56041616759b78b685e9f7da5b7cba16ea7617
SOUMENDU MUKHERJEE <box4soumendu@gmail.com>
2021-05-12 11:21:07 +0530

    database configuration and connection added

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
 
 //Set the view engine
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: false }));
+
+//Connct to the database
+connectDB();
+
 app.use(express.json({ extented: false }));
 
 const PORT = process.env.PORT || 2021;
